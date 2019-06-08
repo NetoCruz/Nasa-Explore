347ced01a2f7056c489d39002c7a10bf293c69ac
unknown <galaxy.net0710@gmail.com>
2019-06-07 21:10:58 -0500

    Agregando estilos

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import NasaApi from "./components/Nasa-Api";
 import Rover from "./components/rover";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 function App() {
   return (
     <div>
+      <Navbar />
+      <Header />
       <NasaApi />
       <Rover />
     </div>
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import "./css/index.css";
 class Photos extends Component {
   render() {
     return (
       <div>
-        <img src={this.props.img} />
-        <div>{this.props.character}</div>
-        <div>{this.props.explanation}</div>
+        <section class="showcase">
+          <div class="container-fluid p-0">
+            <div class="row no-gutters">
+              <div class="col-lg-6 order-lg-2 text-white showcase-img">
+                <img src={this.props.img} />
+              </div>
+              <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                <h2>{this.props.character}</h2>
+                <p class="lead mb-0">{this.props.explanation}</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
